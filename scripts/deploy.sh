656ae3dd18bdd8322ed06a18b877814e468eb85e
JennyI71 <jenny-innes@hotmail.co.uk>
2025-05-03 19:36:56 +0100

    changes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-# Install dependencies
-echo "Installing Java 17..."
-sudo yum install -y java-17-amazon-corretto
-
 # Navigate to the app directory
-mkdir -p /home/ec2-user/app
 cd /home/ec2-user/app
 
 # Stop any existing running Java processes
 echo "Stopping existing Java app if it's running..."
 ps aux | grep 'DevOpsSummative-1.0-SNAPSHOT.jar' | grep -v grep | awk '{print $2}' | xargs -r kill -9
-
-# Move the JAR file if needed
-echo "Moving new JAR file..."
-cp /home/ec2-user/scripts/DevOpsSummative-1.0-SNAPSHOT.jar .
 
 # Make the JAR file executable
 chmod +x DevOpsSummative-1.0-SNAPSHOT.jar
@@ -22,6 +13,9 @@ chmod +x DevOpsSummative-1.0-SNAPSHOT.jar
 # Start the new Java app in the background
 echo "Starting new Java app..."
 nohup java -jar DevOpsSummative-1.0-SNAPSHOT.jar > app.log 2>&1 &
+
+# Wait a few seconds for the app to start
+sleep 5
 
 # Validate the app
 echo "Validating the service..."
